8c601099f859f3650cbb527398ade116ed4442fc
Deepak028 <47886299+Deepak028@users.noreply.github.com>
2019-03-29 15:00:48 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,9 @@
+def function(a):
+	if a<0:
+		print("the value is negative",a)
+	elif a>0:
+		print("the value is positive",a)
+	elif a==0:
+		print("the value 0")
+b=int(input("enter the value you desire"))
+function(b)
